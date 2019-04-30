6c96576b39d3da9bfe7b3236f1867c7696a8de6b
THUYShao <shaoyunqiu14@gmail.com>
2019-04-30 09:57:28 +0800

    update taskUrl

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ var baseUrl = "http://127.0.0.1:8000";
 // var baseUrl = "http://166.111.138.86:15016";
 var checkUrl = baseUrl + "/user/check/"; // verify user
 var dataUrl = baseUrl + "/exp_domain_expertise/data/"; // save data
-var taskUrl = baseUrl + "exp_domain_expertise/task_type/"
+var taskUrl = baseUrl + "/exp_domain_expertise/task_type/"
 var username, password;
 var version = "1.0";
 var debug1 = false;
