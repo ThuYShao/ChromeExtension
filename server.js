be5de27635387491af8a0682855cb55c41d0175e
THUYShao <shaoyunqiu14@gmail.com>
2019-04-30 21:22:40 +0900

    add console log in server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-var baseUrl = "http://127.0.0.1:8000";
+//var baseUrl = "http://127.0.0.1:8000";
 // var baseUrl = "http://166.111.138.86:15016";
-var checkUrl = baseUrl + "/user/check/"; // verify user
-var dataUrl = baseUrl + "/exp_domain_expertise/data/"; // save data
-var taskUrl = baseUrl + "/exp_domain_expertise/task_type/"; //get task type (if right)
-var username, password;
-var debug = true; 
+//var checkUrl = baseUrl + "/user/check/"; // verify user
+//var dataUrl = baseUrl + "/exp_domain_expertise/data/"; // save data
+//var taskUrl = baseUrl + "/exp_domain_expertise/task_type/"; //get task type (if right)
+//var username, password;
+//var debug = true; 
+
+if (debug) console.log('Server Mainpage is loaded')
 
 // get right type save in local storage
 function get_task_type(){
     var server_url = window.location.href;
     chrome.runtime.sendMessage({task_type: "request", url: server_url}, function (response) {
+        if (debug) console.log(response); 
         if (response.task_type == 1) {
             if (debug) {
                 console.log('get task_type');
